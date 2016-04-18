6cac4c04fcac8d0266c5b889ad22a898a6d01118
jgeigerm <jmg2967@rit.edu>
2016-04-18 02:21:21 -0400

    hex

--- searchstring.sh
+++ searchstring.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 AAPT_PATH="./aapt"
 
-
-AAPT_PATH=$(echo $(cd $(dirname "$AAPT_PATH") && pwd -P)/$(basename "$AAPT_PATH"))
-APK_PATH=$(echo $(cd $(dirname "$1") && pwd -P)/$(basename "$1"))
-if [ $# -ne 2 ]; then
-    echo "$0 apk <resource number (decimal)>"
+if [ $# -gt 3 -o $# -lt 2 ]; then
+    echo "$0 the.apk <resource number (hex OR decimal)>"
     DIE=1
 fi
+#http://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+while [[ $# > 0 ]]; do
+    if [ -z "$APK_PATH" ]; then
+        APK_PATH=$1
+    elif [ -z $RESOURCE ]; then
+        RESOURCE=$1
+        if [ "${RESOURCE:0:2}" == "0x" ]; then
+            HEX=1
+        fi
+    fi
+    shift
+done
+
+AAPT_PATH=$(echo $(cd $(dirname "$AAPT_PATH") && pwd -P)/$(basename "$AAPT_PATH"))
+APK_PATH=$(echo $(cd $(dirname "$APK_PATH") && pwd -P)/$(basename "$APK_PATH"))
 if [ ! -f "$AAPT_PATH" ]; then
     echo "aapt cannot be found, edit AAPT_PATH with the correct path"
     DIE=1
@@ -20,4 +32,8 @@ if [ ! -z $DIE ]; then
     exit
 fi
 
-"$AAPT_PATH" d --values resources "$APK_PATH" | grep -v "spec " | grep -A1 $(printf "0x%x" $2) | tail -1 | awk -F \" '{print $2}'
+if [ -z "$HEX" ]; then
+    "$AAPT_PATH" d --values resources "$APK_PATH" | grep -v "spec " | grep -A1 $(printf "0x%x" $RESOURCE) | tail -1 | awk -F \" '{print $2}'
+else
+    "$AAPT_PATH" d --values resources "$APK_PATH" | grep -v "spec " | grep -A1 $RESOURCE | tail -1 | awk -F \" '{print $2}'
+fi
